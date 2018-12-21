8e2a086b51a63c66e0d7bb20c5d02ee702fa8a7a
Adrien Brignon <contact@ethaligan.fr>
2018-12-21 21:09:53 +0100

    Fixed my_atol and my_atoi tests

--- tests/stdlib/my_atoi.c
+++ tests/stdlib/my_atoi.c
@@ -6,17 +6,11 @@
 */
 
 #include "my.h"
-#include <limits.h>
 #include <criterion/criterion.h>
 
 Test(unit, my_atoi)
 {
-    char max[10 + 1];
-    char min[10 + 1];
-
-    my_itoa(INT_MIN, min);
-    my_itoa(INT_MAX, max);
-
-    cr_assert_eq(my_atoi(min), INT_MIN);
-    cr_assert_eq(my_atoi(max), INT_MAX);
+    cr_assert_eq(my_atoi("0"), 0);
+    cr_assert_eq(my_atoi("1"), 1);
+    cr_assert_eq(my_atoi("-1"), -1);
 }
--- tests/stdlib/my_atol.c
+++ tests/stdlib/my_atol.c
@@ -6,17 +6,11 @@
 */
 
 #include "my.h"
-#include <limits.h>
 #include <criterion/criterion.h>
 
 Test(unit, my_atol)
 {
-    char max[18 + 1];
-    char min[18 + 1];
-
-    my_ltoa(LONG_MIN, min);
-    my_ltoa(LONG_MAX, max);
-
-    cr_assert_eq(my_atol(min), LONG_MIN);
-    cr_assert_eq(my_atol(max), LONG_MAX);
+    cr_assert_eq(my_atol("0"), 0L);
+    cr_assert_eq(my_atol("1"), 1L);
+    cr_assert_eq(my_atol("-1"), -1L);
 }
