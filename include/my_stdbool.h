60f2ff9c5056bc3ce6325971edc6f64f7189d58e
Adrien Brignon <contact@ethaligan.fr>
2018-12-23 11:37:56 +0100

    Added my_stdbool header

---
+++ include/my_stdbool.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** libmy
+** File description:
+** Boolean type.
+*/
+
+#ifndef MY_STDBOOL_H_
+#define MY_STDBOOL_H_
+
+typedef enum {
+    my_false = 0,
+    my_true = !my_false
+} my_bool;
+
+#endif
