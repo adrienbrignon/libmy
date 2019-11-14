c2b607677a1c9a8b0f37489984d1591cd06124c2
Adrien Brignon <adrien@brignon.dev>
2019-11-14 10:02:46 +0100

    Removed printf function

--- include/my_stdio.h
+++ include/my_stdio.h
@@ -12,24 +12,7 @@
 
 #include "my_config.h"
 
-#ifndef MY_PRINTF_CALLBACK
-#define MY_PRINTF_CALLBACK(x) (my_printf_callback_ ## x)
-#endif
-
-typedef int (my_printf_callback_t)(va_list *list);
-
-typedef struct my_printf_map {
-    char specifier;
-    my_printf_callback_t *callback;
-} my_printf_map_t;
-
 MY_API int my_putchar(char c);
 MY_API int my_puts(const char *str);
-MY_API int my_printf(const char *format, ...);
-
-MY_API int my_printf_call(char c, va_list *list);
-MY_API int my_printf_callback_s(va_list *list);
-MY_API int my_printf_callback_d(va_list *list);
-MY_API int my_printf_callback_c(va_list *list);
 
 #endif
